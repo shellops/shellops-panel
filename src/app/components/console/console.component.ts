2c4b7d720551e344d12a1e1c87ef5b78143c8226
Mohsen Esmaeili <m-esm@hotmail.com>
2021-01-26 13:58:44 +0330

    feat: integrate with xterm

--- src/app/components/console/console.component.ts
+++ src/app/components/console/console.component.ts
@@ -1,33 +1,21 @@
 import { ThrowStmt } from '@angular/compiler';
-import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ShellNode } from '../../interfaces/shell-node.interface';
 import { WsPayload } from '../../interfaces/ws-payload.interface';
 import { WsService } from '../../shared/ws.service';
-import * as AnsiConverter from "ansi-to-html";
 import { DomSanitizer } from '@angular/platform-browser';
 import { ShellService } from '../../shared/shell.service';
-
-interface ConsoleCommand {
-  command: string,
-  logs: {
-    raw: string,
-    date: Date,
-    output: string | any,
-    type: string
-  }[],
-}
+import { Terminal } from 'xterm';
 
 @Component({
   selector: 'app-console',
   templateUrl: './console.component.html',
   styleUrls: ['./console.component.scss']
 })
-export class ConsoleComponent implements OnInit, OnDestroy {
+export class ConsoleComponent implements AfterViewInit, OnDestroy {
 
-  ansiCoverter = new AnsiConverter();
-
-  @ViewChild('console') consoleRef: { nativeElement: HTMLElement };
+  @ViewChild('terminal') terminalRef: { nativeElement: HTMLElement };
 
   public get model(): ShellNode {
     return this.shellService.selectedNode;
@@ -35,62 +23,35 @@ export class ConsoleComponent implements OnInit, OnDestroy {
 
   wsSub: Subscription;
 
-  history: ConsoleCommand[] = [];
-
-  current: ConsoleCommand;
-
   constructor(
     private readonly shellService: ShellService,
-    private readonly sanitizer: DomSanitizer,
     private readonly wsService: WsService) { }
 
 
-  ngOnInit(): void {
+  async ngAfterViewInit() {
+
+    const terminal = new Terminal({
+
+    });
+
+    terminal.open(this.terminalRef.nativeElement);
 
     this.wsSub = this.wsService.events.subscribe((next: WsPayload) => {
 
-      // if (this.host && this.host === next.data.host) return;
+      const chunk = next.data.output || next.data.error;
 
-      if (this.current?.logs?.length && next.data.output === '\u001b')
-        return;
+      terminal.write(chunk);
 
-      if (this.current?.command !== next.data.command) {
-
-        if (this.current?.command)
-          this.history.push(this.current);
-
-        this.current = {
-          command: next.data.command,
-          logs: []
-        };
-      }
-
-      const payload = {
-        type: next.data.output ? 'info' : next.data.error ? 'error' : null,
-        date: new Date(),
-        raw: next.data.output || next.data.error,
-        output: this.sanitizer.bypassSecurityTrustHtml(
-          this.ansiCoverter.toHtml(next.data.output || next.data.error)
-        )
-      };
-
-      if (this.current?.logs?.length && payload.raw.startsWith('\u001b'))
-        Object.assign(this.current.logs[this.current.logs.length - 1], payload)
-      else
-        this.current.logs.push(payload);
-
-
-      setTimeout(() => {
-        this.consoleRef.nativeElement.scrollTo({
-          top: this.consoleRef.nativeElement.scrollHeight
-        })
-      }, 100);
+      localStorage.setItem('terminal_history', JSON.stringify([
+        ...(JSON.parse(localStorage.getItem('terminal_history') || '[]')),
+        chunk
+      ]));
 
     });
 
   }
 
-  ngOnDestroy(): void {
+  ngOnDestroy() {
     this.wsSub?.unsubscribe();
   }
 
