b978d714352b36204d389c74a06a4b2fba76a2ee
Mohsen Esmaeili <m-esm@hotmail.com>
2021-02-08 04:45:23 +0330

    feat: logo component

---
+++ shellops-next-panel/components/layout/logo.tsx
@@ -0,0 +1,10 @@
+import styles from "./logo.module.scss";
+export default function Logo() {
+  return (
+    <a className={styles.logo} href="/">
+      {" "}
+      <img src="logo.svg" alt="" />
+      <h1>Shellops</h1>
+    </a>
+  );
+}
