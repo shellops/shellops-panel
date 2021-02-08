75c7dff49f12638a88f57271db0d913a1b1e59ee
Mohsen Esmaeili <m-esm@hotmail.com>
2021-02-09 02:52:04 +0330

    fix: use router for logo link

--- components/layout/logo.tsx
+++ components/layout/logo.tsx
@@ -1,7 +1,10 @@
+import { useRouter } from "next/router";
 import styles from "./logo.module.scss";
 export default function Logo() {
+  const router = useRouter();
+
   return (
-    <a className={styles.logo} href="/">
+    <a className={styles.logo} onClick={() => router.replace("/")}>
       {" "}
       <img src="/logo.svg" alt="" />
       <h1>Shellops</h1>
