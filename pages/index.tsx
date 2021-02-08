1e4a4ee1bd183810811f1b277cc43b69746d0dcf
Mohsen Esmaeili <m-esm@hotmail.com>
2021-02-09 02:51:11 +0330

    fix: index page is list only

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 
 import Machine from "../components/core/machine";
@@ -7,65 +8,40 @@ import useLiveMachinesEffect, {
   getUrlTokens,
 } from "../lib/live-machines.effect";
 import removeMachine from "../lib/remove-machine";
-import saveMachine from "../lib/save-machine";
 import styles from "./index.module.scss";
 
-const Home: NextPage<any> = ({}: AppProps) => {
-
-  let [addMode, toggleAddMode] = useState(false);
-  const [urlToken, urlTokenChange] = useState("");
-  const [machines, machinesChange] = useState([]);
+const Home: NextPage<any> = ({ machines }: AppProps) => {
   const [urlTokens, urlTokensChange] = useState([]);
 
+  const router = useRouter();
+
   useEffect(() => {
-    urlTokensChange(getUrlTokens());
-    return () => {};
+    const urls = getUrlTokens();
+    if (!urls?.length) router.push("/add");
+    else urlTokensChange(getUrlTokens());
   }, []);
 
-  if (!addMode && !urlTokens?.length) addMode = true;
+  return (
+    <>
+      <div className={styles.bg}></div>
+      <div className={styles.connect}>
+        <div className="container">
+          <div className={styles.list}>
+            {urlTokens?.map((urlToken) => (
+              <Machine
+                key={urlToken}
+                urlToken={urlToken}
+                machine={machines?.find((p) => p.urlToken === urlToken)}
+                removeMachine={() => removeMachine(urlToken, urlTokensChange)}
+              />
+            ))}
 
-  const handleSaveMachine = () => {
-    if (!urlToken || urlTokens?.find((p) => p === urlToken)) return;
-    saveMachine(urlToken, urlTokensChange);
-  };
-  // * Handle machine changes, refresh every 3 second
-  useLiveMachinesEffect(  machinesChange);
-
-  const add = (
-    <section>
-      <p>Install Shellops agent on your machine</p>
-      <pre>npm i -g shellops</pre>
-      <p>Start agent</p>
-      <pre>shellops start</pre>
-      <p>Paste printed URL Token</p>
-      <input
-        type="text"
-        value={urlToken}
-        onChange={(e) => urlTokenChange(e.target.value)}
-        placeholder="URL Token here, EX: http://client:secret@1.2.3.4"
-      />
-      <button onClick={handleSaveMachine}>Save and Connect</button>
-    </section>
-  );
-
-  const list = (
-    <div className="container">
-      <div className={styles.list}>
-        {urlTokens?.map((urlToken) => (
-          <Machine
-            key={urlToken}
-            urlToken={urlToken}
-            machine={machines?.find((p) => p.urlToken === urlToken)}
-            removeMachine={() => removeMachine(urlToken, urlTokensChange)}
-          />
-        ))}
-
-        <Machine key="add" onAddMode={() => toggleAddMode(true)} />
+            <Machine key="add" onAddMode={() => router.push("/add")} />
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
-
-  return <div className={styles.connect}>{addMode ? add : list}</div>;
 };
 
 export default Home;
