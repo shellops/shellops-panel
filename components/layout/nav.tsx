c35bf702ff34fceb5477089867c360239f1e2340
Mohsen Esmaeili <m-esm@hotmail.com>
2021-02-09 23:21:55 +0330

    fix: nav different states

--- components/layout/nav.tsx
+++ components/layout/nav.tsx
@@ -1,8 +1,9 @@
 import { useRouter } from "next/router";
 import { AppProps } from "../../lib/interfaces/app-props.interface";
+import Logout from "../../lib/logout";
 import styles from "./nav.module.scss";
 
-export default function Nav({ machine, user }: AppProps) {
+export default function Nav({ machine, machines, user }: AppProps) {
   const router = useRouter();
 
   const distro = (
@@ -15,33 +16,65 @@ export default function Nav({ machine, user }: AppProps) {
   return (
     <nav className={styles.nav}>
       <ul>
-        <li className={styles.back} onClick={() => router.back()}>
-          <img src="/icons/solid/chevron-left.svg" alt="" />
-        </li>
+        {machine ? (
+          <>
+            <li className={styles.back} onClick={() => router.replace("/")}>
+              <img src="/icons/solid/chevron-left.svg" alt="" />
+              Machines
+            </li>
 
-        <li className={styles.host}>
-          {distro}
-          {machine?.general?.system?.version}
-          {machine?.general?.system?.virtual ? "(VIRTUAL)" : ""} -{" "}
-          {machine?.urlToken?.split("@")[1].split(":")[0]}
-        </li>
+            <li className={styles.host}>
+              {distro}
+              {machine?.general?.system?.version}
+              {machine?.general?.system?.virtual ? "(VIRTUAL)" : ""} -{" "}
+              {machine?.hostname}
+            </li>
+            <li onClick={() => router.replace(`/machines/${machine.hostname}`)}>
+              <img src="/icons/solid/info-circle.svg" alt="" />
+              Info
+            </li>
+
+            <li
+              onClick={() =>
+                router.replace(`/machines/${machine.hostname}/store`)
+              }
+            >
+              <img src="/icons/solid/shopping-cart.svg" alt="" />
+              Store
+            </li>
+
+            <li>
+              <img src="/icons/solid/cubes.svg" alt="" />
+              Apps
+            </li>
+          </>
+        ) : (
+          <>
+            <li
+              onClick={() =>
+                router.replace(machines.length ? "/" : "/machines/add")
+              }
+            >
+              <img src="/icons/solid/server.svg" alt="" />
+              Machines
+            </li>
+          </>
+        )}
 
         <li>
-          <img src="/icons/solid/info-circle.svg" alt="" />
-          Info
-        </li>
-
-        <li>
-          <img src="/icons/solid/cubes.svg" alt="" />
-          Apps
-        </li>
-
-        <li>
-          <img src="/icons/solid/shopping-cart.svg" alt="" />
-          Store
-        </li>
-        <li>
-          Logged in as <b>{user?.displayName || 'N/A'}</b>
+          <img src="/icons/solid/user-secret.svg" alt="" />
+          {user?.displayName}{" "}
+          <img
+            className={styles.caret}
+            src="/icons/solid/caret-down.svg"
+            alt=""
+          />
+          <ul>
+            <li onClick={() => Logout()}>
+              <img src="/icons/solid/sign-out-alt.svg" alt="" />
+              Logout
+            </li>
+          </ul>
         </li>
       </ul>
     </nav>
