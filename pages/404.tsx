b4717137e01fa8aa24cfacba4a2bbbc89316faf1
Mohsen Esmaeili <m-esm@hotmail.com>
2021-02-10 13:17:19 +0330

    fix: 404 image path

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 export default function FourOhFour() {
   return (
     <div style={{ textAlign: "center", padding: 45 }}>
-      <img src="404.svg" style={{ maxWidth: 320, marginBottom: 30 }} alt="" />
+      <img src="/404.svg" style={{ maxWidth: 320, marginBottom: 30 }} alt="" />
       <h1 style={{ fontWeight: "bold", fontSize: 17, marginBottom: 10 }}>
         404 - Page Not Found
       </h1>
