0e4ab7192823398bfb2efa0a7f2ad0f49f158e66
Mohsen Esmaeili <m-esm@hotmail.com>
2021-02-24 05:52:17 +0330

    fix: csp meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -109,6 +109,10 @@ const PanelApp = ({ Component, pageProps }) => {
       <Head>
         <title>Shellops</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="default-src * self blob: data: gap:; style-src * self 'unsafe-inline' blob: data: gap:; script-src * 'self' 'unsafe-eval' 'unsafe-inline' blob: data: gap:; object-src * 'self' blob: data: gap:; img-src * self 'unsafe-inline' blob: data: gap:; connect-src self * 'unsafe-inline' blob: data: gap:; frame-src * self blob: data: gap:;"
+        />
       </Head>
       <header>
         <Logo></Logo>
