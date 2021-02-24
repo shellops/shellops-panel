e15ee67aeee55e31f858e6cf78caf12d8af892c7
Mohsen Esmaeili <m-esm@hotmail.com>
2021-02-24 05:31:29 +0330

    fix: fetch machine content type and options

--- lib/fetch-machine.ts
+++ lib/fetch-machine.ts
@@ -1,13 +1,14 @@
-export default async function fetchMachine(path: string, urlToken: string, method = 'GET'): Promise<any> {
+export default async function fetchMachine(path: string, urlToken: string, method = 'GET', body?): Promise<any> {
 
     const headers = new Headers();
     const userPass = urlToken.split('//')[1].split('@')[0];
 
     headers.append('Authorization', 'Basic ' + btoa(userPass));
+    headers.append('Content-Type', 'application/json');
 
     const response = await fetch(
         urlToken.replace(userPass, '') + path,
-        { headers, method });
+        { headers, method, body: body ? JSON.stringify(body) : undefined });
 
     if (response.status > 400) throw {
         status: response.status,
